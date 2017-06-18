4d1d447a8c290b939ffe6b866a67e450e06029f9
Benjamin Spille <benjaminspille@Benjamins-MBP.home>
2017-06-17 20:53:19 -0400

    made some minor changes for heroku deployment local test ok

--- controllers/controller.js
+++ controllers/controller.js
@@ -55,41 +55,46 @@ var express = require("express"),
       //     subject = token.sub;
           // replace 1 with subject for later use with http requests
           // 1 is the seed tokenSub created for querying the data collection
-      twilio.pulse(1);
+      twilio.pulse('1');
     });
 
     // route to render the welcome page
     router.get("/", (req, res) => {
-      var googleAuthToken = "1";//"eyJhbGciOiJSUzI1NiIsImtpZCI6ImEyOThhNTZiNmFjMDU0MzEyNTNkNDkwMzA4MTZhNWViZjk5YTEzYzUifQ.eyJhenAiOiI1MzM1MjQzMzk2MTMtbW0zdjcwb25xMzEwdnIwcWVwMml0MnBqNXZjajF0MzMuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI1MzM1MjQzMzk2MTMtbW0zdjcwb25xMzEwdnIwcWVwMml0MnBqNXZjajF0MzMuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTM4ODIxNjEyOTk0NDc2MDA4MTkiLCJlbWFpbCI6Impvc2hjYnV0bGVyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiT3hQWVdSS3Vtd0ZvODJ5ZmJ6S3VJdyIsImlzcyI6ImFjY291bnRzLmdvb2dsZS5jb20iLCJpYXQiOjE0OTc2NzczMTMsImV4cCI6MTQ5NzY4MDkxMywibmFtZSI6Ikpvc2ggQnV0bGVyIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS8teGl2am84LWlYUTQvQUFBQUFBQUFBQUkvQUFBQUFBQUFRLTAvR2EwajB1ZF9NMFUvczk2LWMvcGhvdG8uanBnIiwiZ2l2ZW5fbmFtZSI6Ikpvc2giLCJmYW1pbHlfbmFtZSI6IkJ1dGxlciIsImxvY2FsZSI6ImVuIn0.2LFtX5mmKCDGh9vMha-xpenFE2B5aT_s_3nZF7za5e4JwSo2K6c4CGaKG2QsWEAXny1ucsHbaq3JqzmgJ5SvYWG8eEu03lBbVeVhivZfft-W7s6fixTqtvxcE6MBPvFPEIF7VIFlJOcuqRrj5Lpo8WUUq6bhCcDbAhEK_aHZd4oWHcn5PbYNme8lUMWWBOlC7xOqYdKWVdjWlthjo4w1M7BbJKfIhQ_CaKU4kIfkTdZmIvqigU28JMmGh7C3lT3FaVh8ugAE8myAzKnSmbWNQUM6vStbHmhkdEagIXFZT0ezxnErCK0Ccy_k7dAwcxXvuTRJj1nxPXFqAZQphxvj3w";
-      // TODO #GoogleAuth - Get token from request Google Auth session
-      //var googleAuthToken = req.<google auth>;
-      crud.read(
-        googleAuthToken, 
-        (users) => {
-          if (!users) {
-            console.error("No response from database");
-            res.statusCode = 500; // TODO choose proper code (internal server error)
-            res.end();
-            return;
-          }
-          console.log("users=%j", users);
-          // TODO decide what to do if multiple users have the same token
-          // possible assert... 
-          if (users.length > 1) {
-            console.warn("%d users with same auth token", users.length);  
-          }
-          var user = users[0];
-          if (user) {
-            var scriptParams = {
-              givenName: user.givenName
-            };
-            res.render("index", scriptParams);
-          } else { // No User for auth token
-            console.log("User Fetch Failed - No User for auth token = %s", googleAuthToken);
-            res.end();
-          }
-        }
-      );
+      // this is all that is need here
+      res.render("index");
+
+      // total over kill google auth returns profile information on the front end
+      // and checking the collection is already drafted in /user
+      // var googleAuthToken = "1";//"eyJhbGciOiJSUzI1NiIsImtpZCI6ImEyOThhNTZiNmFjMDU0MzEyNTNkNDkwMzA4MTZhNWViZjk5YTEzYzUifQ.eyJhenAiOiI1MzM1MjQzMzk2MTMtbW0zdjcwb25xMzEwdnIwcWVwMml0MnBqNXZjajF0MzMuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI1MzM1MjQzMzk2MTMtbW0zdjcwb25xMzEwdnIwcWVwMml0MnBqNXZjajF0MzMuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTM4ODIxNjEyOTk0NDc2MDA4MTkiLCJlbWFpbCI6Impvc2hjYnV0bGVyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJhdF9oYXNoIjoiT3hQWVdSS3Vtd0ZvODJ5ZmJ6S3VJdyIsImlzcyI6ImFjY291bnRzLmdvb2dsZS5jb20iLCJpYXQiOjE0OTc2NzczMTMsImV4cCI6MTQ5NzY4MDkxMywibmFtZSI6Ikpvc2ggQnV0bGVyIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS8teGl2am84LWlYUTQvQUFBQUFBQUFBQUkvQUFBQUFBQUFRLTAvR2EwajB1ZF9NMFUvczk2LWMvcGhvdG8uanBnIiwiZ2l2ZW5fbmFtZSI6Ikpvc2giLCJmYW1pbHlfbmFtZSI6IkJ1dGxlciIsImxvY2FsZSI6ImVuIn0.2LFtX5mmKCDGh9vMha-xpenFE2B5aT_s_3nZF7za5e4JwSo2K6c4CGaKG2QsWEAXny1ucsHbaq3JqzmgJ5SvYWG8eEu03lBbVeVhivZfft-W7s6fixTqtvxcE6MBPvFPEIF7VIFlJOcuqRrj5Lpo8WUUq6bhCcDbAhEK_aHZd4oWHcn5PbYNme8lUMWWBOlC7xOqYdKWVdjWlthjo4w1M7BbJKfIhQ_CaKU4kIfkTdZmIvqigU28JMmGh7C3lT3FaVh8ugAE8myAzKnSmbWNQUM6vStbHmhkdEagIXFZT0ezxnErCK0Ccy_k7dAwcxXvuTRJj1nxPXFqAZQphxvj3w";
+      // // TODO #GoogleAuth - Get token from request Google Auth session
+      // //var googleAuthToken = req.<google auth>;
+      // crud.read(
+      //   googleAuthToken,
+      //   (users) => {
+      //     if (!users) {
+      //       console.error("No response from database");
+      //       res.statusCode = 500; // TODO choose proper code (internal server error)
+      //       res.end();
+      //       return;
+      //     }
+      //     console.log("users=%j", users);
+      //     // TODO decide what to do if multiple users have the same token
+      //     // possible assert...
+      //     if (users.length > 1) {
+      //       console.warn("%d users with same auth token", users.length);
+      //     }
+      //     var user = users[0];
+      //     if (user) {
+      //       var scriptParams = {
+      //         givenName: user.givenName
+      //       };
+      //       res.render("index", scriptParams);
+      //     } else { // No User for auth token
+      //       console.log("User Fetch Failed - No User for auth token = %s", googleAuthToken);
+      //       res.end();
+      //     }
+      //   }
+      // );
     });
 
 // export router here
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express"),
     bodyParser = require("body-parser"),
     exphbs = require("express-handlebars"),
     mongoose = require("mongoose"),
+    // required for first seed
+    seed = require("./test/seed.js"),
     // Sets up the Express App
     app = express(),
     PORT = process.env.PORT || 3000;
@@ -18,7 +20,10 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Database configuration with mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/pulse");
+// localhost connection
+// mongoose.connect("mongodb://localhost/pulse");
+// heroku mLabs connection
+mongoose.connect("mongodb://heroku_nvjgbw4q:f10viekoqjf0va83nia9bpjq9f@ds131119.mlab.com:31119/heroku_nvjgbw4q")
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -29,6 +34,8 @@ db.on("error", function(error) {
 // Once logged in to the db through mongoose, log a success message
 db.once("open", function() {
   console.log("Mongoose connection successful.");
+  // first seed call
+  seed.firstSeed();
 });
 
 // router
