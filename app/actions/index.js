47a29b25ddb8de2e5c99df334837ec4bba834dad
Benjamin Spille <benjaminspille@Benjamins-MBP.home>
2017-07-02 21:07:43 -0400

    started to list out the actions needed to make changes to the redux state

---
+++ app/actions/index.js
@@ -0,0 +1,15 @@
+// actions that descride the changes to the state
+// contacts are objects that get added to a array of objects
+// select and unselect toggle is active contact
+// contacts will need a key value to reference them
+
+set_pin
+set_phone_number
+add_contact
+select_contact
+get_contacts
+get_pin
+get_phone_number
+unselect_constact
+add_access_token
+add_image_url
