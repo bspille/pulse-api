e0f20b1fd5a01f0de3ed8acee94c9bbae36eb007
Chris <chriscallanjr@me.com>
2017-07-03 13:05:20 -0400

    refactoring components

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,16 +1,12 @@
-// redo this as a functional component
 import React from 'react';
 
+const Footer = () => (
+    <section>
+        <div className="push"></div>
+        <div id="pulse-footer">
+            <p>© Pulse / All Rights Reserved.</p>
+        </div>
+    </section>
+);    
 
-export default (props) =>{
-
-   
-        return (
-            <section>
-                <div className="push"></div>
-                <div id="pulse-footer">
-                    <p>© Pulse / All Rights Reserved.</p>
-                </div>
-            </section>
-        );
-}
+export default Footer;
