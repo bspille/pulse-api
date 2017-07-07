1661eaf9c2f356fb1b7a3e8567bf011cd8fecd22
Joshua Butler <joshcbutler@gmail.com>
2017-07-07 11:59:47 -0400

    made pulse-button more responsive, removed rogue footer text

--- app/components/footer.js
+++ app/components/footer.js
@@ -7,7 +7,6 @@ import * as actionCreators from '../actions/index';
 export default (props) => { 
         return (
         <div className="mobile-bottom-bar">
- front-end-styling-josh
         <a href="#" className="footer-link">
         <a href="/"><i className="fa fa-sign-out footer-icon"></i><span className='footer-text'>Log out</span></a>
         </a>
